d38c3968b036e271de38a0d0878bd1952b52a474
techmedaddy <umarejazimam69@gmail.com>
2026-05-31 14:16:35 +0530

    chore: clean up repo by removing unwanted and scratch files

--- scratch_logs.js
+++
@@ -1,7 +0,0 @@
-const { execSync } = require('child_process');
-try {
-  const logs = execSync('docker compose logs --tail 200 backend').toString();
-  console.log(logs);
-} catch (e) {
-  console.log(e.message);
-}
